1209db10957aed1f099ceeeb43ae4114f9f9afe1
Zachary Jensen <ZJTJensen@yahoo.com>
2018-03-15 17:52:52 -0400

    finished three card about to work on 6 and 12

--- js/style.js
+++ js/style.js
@@ -6,18 +6,19 @@ $(document).ready(function(){
         var blue = 0;
         var turn;
         var start = Math.floor(Math.random() * 2) + 1;
-        
+        var flip = new Audio('sounds/flip.mp3');
 
     $("#start-button").click(function(num){
         if(start == 1 || turn =="Blue"){
             turn = "Blue";
             document.getElementById('page').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
             document.getElementById('full').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
-          
+            document.getElementById("who-turn").innerHTML = "<h1 style= 'color:blue;'>Turn: "+ turn +"</h1>";
         }else if(start == 2 || turn =="Red"){
             turn ="Red";
             document.getElementById('page').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
             document.getElementById('full').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
+            document.getElementById("who-turn").innerHTML = "<h1 style= 'color:red;'>Turn: "+ turn +"</h1>";
         }
         var val = document.getElementById('image-number');
         num = val.options[val.selectedIndex].value;
@@ -34,6 +35,8 @@ $(document).ready(function(){
         //     $('#background').animate({width: 'toggle'});
         // }
         $("#game").slideToggle("slow", function(){});
+        document.getElementById("red-score").style= "display:inline-block;"
+        document.getElementById("blue-score").style= "display:inline-block;"
         document.getElementById("game").style= "display:inline-block;"
         function shuffleArray(arr){
             var currentIndex = arr.length, temporaryValue, randomIndex;
@@ -66,7 +69,6 @@ $(document).ready(function(){
                 document.getElementById("cards").innerHTML += "</br>"
             }
             if(num == 6 && (i == 3 || i == 7)){
-                console.log(num)
                 document.getElementById("cards").innerHTML += "</br>"
             }
             if(num == 12 && (i == 5 || i == 11 || i == 17)){
@@ -74,22 +76,30 @@ $(document).ready(function(){
                 document.getElementById("cards").innerHTML += "</br>"
             }
         }
+
+            // to do-- make it so that you recive the whole div, as to amek them display:none
+
         var flipped = 0;
         var choices =[];
-        $(".er3").flip();
-        $(".er3").click(function(){
+        var remove = [];
+        var toFlip = true;
+        if(toFlip == true){
+        $(".er"+num).flip();
+        $(".er"+num).click(function(){
+            flip.play();
             $(this).flip(true)
+            var collection = this
             var img = this.getElementsByClassName('img')
             for (var i = 0; i < img.length; i+= 1) {
                 choices[flipped] = img[i].alt;
+                remove[flipped] = collection;
             }
-            console.log(choices)
-            
-            flipped++
-            console.log(flipped)
+            flipped++;
             if(flipped >= 2){
+                toFlip=false;
+                // var img1 =choices[0].getElementsByClassName('img').alt
+                // var img2 =choices[0].getElementsByClassName('img').alt
                 if(choices[0].includes(choices[1]) || choices[1].includes(choices[0])){
-                    console.log("Winner")
                     if(turn =="Red"){
                         red++;
                         document.getElementById('red-board').innerHTML = red;
@@ -97,27 +107,71 @@ $(document).ready(function(){
                         blue++;
                         document.getElementById('blue-board').innerHTML = blue;
                     }
-                }
-                function time(){
-                    flipped = 0;
-                    console.log('hello')
-                    $(".er3").flip(false)
-                    if(turn =="Red"){
-                        turn = "Blue";
-                        document.getElementById('page').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
-                        document.getElementById('full').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
-                    }else{
-                        turn = "Red";
-                        document.getElementById('page').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
-                        document.getElementById('full').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
-                 
+                    console.log("Here it is")
+                    
+                   
+                    // console.log(test1)
+                    // test1.style ="display:none;"
+                    //document.remove[1].getElementsByClassName("er"+num).style= "display:none;";
+                    var divs = document.getElementsByClassName("er"+num);
+                    setTimeout(hello,1000)
+                    function hello(){
+                    for (var j=0; j < divs.length; j++){
+                        if (divs[j] == remove[0]||divs[j] == remove[1]){
+                            divs[j].style = "display:none;"
+                         }
+                     }
+                    }
+
+                    if(red + blue == num){
+                        document.getElementById("red-score").style= "display:none;"
+                        document.getElementById("blue-score").style= "display:none;"
+                        document.getElementById("game").style = "display:none;"
+                        document.getElementById("winner").style = "display:inline-block;"
+                        if (red > blue){
+                            document.getElementById('page').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
+                            document.getElementById('full').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '                
+                            document.getElementById('winner').innerHTML = "<h1 id = 'winning-color' style ='color:blue'>Winner</br> BLUE</h1><button id ='buttonRefresh'>Re-Start</button>"
+                        }else{
+                            document.getElementById('page').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
+                            document.getElementById('full').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
+                            document.getElementById('winner').innerHTML = "<h1 id = 'winning-color' style ='color:red'>Winner</br> Red</h1><button id ='buttonRefresh'>Re-Start</button>"
+
+                        }
                     }
                 }
-                setTimeout(time, 1000)
+                    setTimeout(time, 1200)
             }
         });
+    }
+    $("#buttonRefresh").click(function(){
+        location.reload();
+    });
+        function time(){
+            flipped = 0;
+            remove =[];
+           
+            $(".er"+num).flip(false)
+            flip.play();
+            if(turn =="Red"){
+                turn = "Blue";
+                document.getElementById('page').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
+                document.getElementById('full').style ='background: linear-gradient(to bottom right, rgba(255, 0, 0, 0.405), blue); '
+                document.getElementById("who-turn").innerHTML = "<h1 style= 'color:blue;'>Turn: "+ turn +"</h1>";
+            }else{
+                turn = "Red";
+                document.getElementById('page').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
+                document.getElementById('full').style ='background: linear-gradient(to bottom right, red, rgba(0, 0, 255, 0.405)); '
+                document.getElementById("who-turn").innerHTML = "<h1 style= 'color:red;'>Turn: "+ turn +"</h1>";
+            }
+            
 
-        
+            toFlip=true;
+        }
+     
+      
+         
     });
 
 })
+
